5d813a3c7e2e9d8ab502be5408ca11c4d57e95dd
nemonuri <nemonuri2@gmail.com>
2026-01-11 10:57:46 +0900

    first commit

---
+++ src/Nemonuri.DotNetLayoutStudy/ExplicitObjectStruct.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Nemonuri.DotNetLayoutStudy;
+
+[StructLayout(LayoutKind.Explicit)]
+public struct ExplicitObjectStruct
+{
+    [FieldOffset(0)]
+    public object? Obj1;
+
+    [FieldOffset(8)]
+    public object? Obj2;
+
+    public ExplicitObjectStruct(object? obj1, object? obj2)
+    {
+        Obj1 = obj1;
+        Obj2 = obj2;
+    }
+
+    public override string ToString() => $"{nameof(Obj1)} = {Obj1}, {nameof(Obj2)} = {Obj2}";
+}
